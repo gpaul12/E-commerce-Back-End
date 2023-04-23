6d4e33ff9ca4db4940fb35c5f7f625165b32c1ec
Gerry Paul <gpaul12@nc.rr.com>
2023-04-23 13:50:23 -0400

    Tags belongToMany Products (through ProductTag)

--- models/index.js
+++ models/index.js
@@ -19,6 +19,12 @@ Product.belongsToMany(Tag, {
 });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: "tag_id",
+  },
+});
 
 module.exports = {
   Product,
